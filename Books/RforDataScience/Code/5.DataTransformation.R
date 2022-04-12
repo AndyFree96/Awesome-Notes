7e6e08554056a0ffe71b751e5b0d187a216df2ff
andyfree96 <anthonyfree96@gmail.com>
2022-04-12 16:25:46 +0800

    Data transformation 5.2.4 Exercises

--- Books/RforDataScience/Code/5.DataTransformation.R
+++ Books/RforDataScience/Code/5.DataTransformation.R
@@ -48,4 +48,46 @@ filter(df, x > 1)
 
 filter(df, is.na(x) | x > 1)
 
+# 5.2.4 Exercise
+# 1.1
+filter(flights,arr_delay >= 120)
+
+# 1.2
+filter(flights, dest %in% c("IAH", "HOU"))
+
+# 1.3
+filter(flights, carrier %in% c("AA", "UA", "DL")) 
+
+# 1.4
+filter(flights, month %in% c(7, 8, 9))
+
+# 1.5
+filter(flights, (arr_time - sched_arr_time) > 120 & (dep_time == sched_dep_time))
+
+# 1.6
+filter(flights, arr_delay > 120 & dep_delay <= 0)
+       
+# 1.7
+filter(flights, dep_time <= 600 | dep_time == 2400)
+
+# 2
+filter(flights, between(month, 7, 9))
+
+# 3
+filter(flights, is.na(dep_time))
+
+summary(flights)
+
+# 4
+NA ^ 0
+
+NA | TRUE
+
+FALSE & NA
+
+NA * 0
+
+
+
+
 
