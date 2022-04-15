2100c17613fd3a58b67c4b359e469d766311f16a
andyfree96 <anthonyfree96@gmail.com>
2022-04-15 09:49:51 +0800

    Data transformation 5.5.2 Exercises

--- Books/RforDataScience/Code/5.DataTransformation.R
+++ Books/RforDataScience/Code/5.DataTransformation.R
@@ -2,6 +2,18 @@
 library(nycflights13)
 library(tidyverse)
 
+# flights数据集变量含义
+# year, month, day: 起飞日期
+# dep_time, arr_time: 起飞时间和到达时间。格式: HHMM，当地时间。
+# sched_dep_time, sched_arr_time: 计划起飞时间、计划到达时间
+# dep_delay, arr_delay: 起飞延误、到达延误
+# hour, minute: 计划起飞时间拆分为hour和minute
+# carrier: 承运商缩写
+# tailnum: 飞机尾号
+# origin, dest: 始发地、目的地
+# airtime: 空中时间
+# distance: 机场间距
+
 flights
 
 jan1 <- filter(flights, month == 1, day == 1)
@@ -152,3 +164,83 @@ select(flights, contains("TIME"))
 
 select(flights, contains("TIME", ignore.case = FALSE))
 
+
+# mutate函数添加新变量
+flights_sml <- select(flights, 
+                      year:day,
+                      ends_with("delay"),
+                      distance,
+                      air_time)
+mutate(flights_sml, gain = dep_delay - arr_delay,
+       speed = distance / air_time * 60)
+
+mutate(flights_sml, gain = dep_delay - arr_delay,
+       hours = air_time / 60,
+       gain_per_hour = gain / hours)
+
+transmute(flights, gain = dep_delay - arr_delay,
+          hours = air_time / 60,
+          gain_per_hour = gain / hours)
+
+transmute(flights, dep_time, hour = dep_time %/% 100,
+          minute = dep_time %% 100)
+
+(x <- 1:10)
+
+lag(x)
+
+lead(x)
+
+cumsum(x)
+
+cummean(x)
+
+y <- c(1, 2, 2, NA, 3, 4)
+
+min_rank(y)
+
+# 5.5.2 Exercises
+# 1
+time2mins <- function(x){
+  (x %/% 100 * 60 + x %% 100) %% 1440
+}
+
+flights_times <- mutate(flights,
+                        dep_time_mins=time2mins(dep_time),
+                        sched_dep_time_mins=time2mins(sched_dep_time))
+
+select(flights_times, dep_time, dep_time_mins, sched_dep_time,
+       sched_dep_time_mins)
+# 2
+flights_air_time <- mutate(flights, 
+                           dep_time = time2mins(dep_time),
+                           arr_time = time2mins(arr_time),
+                           air_diff=air_time - arr_time + dep_time)
+nrow(filter(flights_air_time, air_diff != 0))
+dim(flights_air_time)
+
+# 3
+select(flights, dep_time, sched_dep_time, dep_delay)
+# dep_time = sched_dep_time + dep_delay
+
+# 4
+flights_delayed <- mutate(flights, 
+                          dep_delay_min_rank = min_rank(desc(dep_delay)),
+                          dep_delay_row_number = row_number(desc(dep_delay)),
+                          dep_delay_dense_rank = dense_rank(desc(dep_delay))
+)
+flights_delayed <- filter(flights_delayed, 
+                          !(dep_delay_min_rank > 10 | dep_delay_row_number > 10 |
+                              dep_delay_dense_rank > 10))
+
+flights_delayed <- arrange(flights_delayed, dep_delay_min_rank)
+# 5
+1:3 + 1:10
+# 报错, 两个向量的长度不匹配
+
+# 6
+sin(1:3)
+
+cos(1:3)
+
+tan(1:3)
