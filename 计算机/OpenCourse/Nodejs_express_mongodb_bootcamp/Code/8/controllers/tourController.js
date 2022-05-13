04074f3ab43541499154d7fc3c605511b9bb94cb
andyfree96 <anthonyfree96@gmail.com>
2022-05-13 08:34:13 +0800

    Express Param Middleware & Chaining Multiple Middleware Functions & Serving Static Files

--- 计算机/OpenCourse/Nodejs_express_mongodb_bootcamp/Code/8/controllers/tourController.js
+++ 计算机/OpenCourse/Nodejs_express_mongodb_bootcamp/Code/8/controllers/tourController.js
@@ -4,8 +4,27 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/tours-simple.json`)
 );
 
+exports.checkID = (req, res, next, val) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  next();
+};
+
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Missing name',
+    });
+  }
+  next();
+};
+
 exports.getTours = (req, res) => {
-  console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
     results: tours.length,
@@ -35,13 +54,6 @@ exports.createTours = (req, res) => {
 };
 
 exports.updateTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-
   res.status(200).json({
     status: 'success',
     data: {
@@ -51,13 +63,6 @@ exports.updateTour = (req, res) => {
 };
 
 exports.deleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-
   res.status(204).json({
     status: 'success',
     data: null,
@@ -69,13 +74,6 @@ exports.getTour = (req, res) => {
 
   const id = req.params.id * 1;
   const tour = tours.find((el) => el.id === id);
-
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
 
   res.status(200).json({
     status: 'success',
--- 计算机/OpenCourse/Nodejs_express_mongodb_bootcamp/Code/8/routes/tourRoutes.js
+++ 计算机/OpenCourse/Nodejs_express_mongodb_bootcamp/Code/8/routes/tourRoutes.js
@@ -3,10 +3,12 @@ const tourController = require('../controllers/tourController');
 
 const tourRouter = express.Router();
 
+tourRouter.param('id', tourController.checkID);
+
 tourRouter
   .route('/')
   .get(tourController.getTours)
-  .post(tourController.createTours);
+  .post(tourController.checkBody, tourController.createTours);
 tourRouter
   .route('/:id')
   .get(tourController.getTour)
