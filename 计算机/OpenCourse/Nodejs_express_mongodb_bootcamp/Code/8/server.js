7f526640c6456f1eef99e779b561a2aa0a8d80ad
andyfree96 <anthonyfree96@gmail.com>
2022-05-14 12:42:45 +0800

    use Mongoose create documents

--- 计算机/OpenCourse/Nodejs_express_mongodb_bootcamp/Code/8/server.js
+++ 计算机/OpenCourse/Nodejs_express_mongodb_bootcamp/Code/8/server.js
@@ -1,9 +1,50 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-
-dotenv.config({ path: './config.env' });
 const app = require('./app-5');
 
-console.log(process.env);
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE_LOCAL;
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection successful!'));
+
+// console.log(process.env);
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+const testTour = new Tour({
+  name: 'The Park',
+  rating: 4.6,
+  price: 457,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 // Start server
 const port = process.env.PORT || 3000;
