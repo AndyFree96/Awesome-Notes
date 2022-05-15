e3358f423fd797c0f39e0be67939cf44b0c9bc4e
andyfree96 <anthonyfree96@gmail.com>
2022-05-15 12:41:26 +0800

    完善Tour Model&将本地文件数据导入Mongo数据库

--- 计算机/OpenCourse/Nodejs_express_mongodb_bootcamp/Code/8/controllers/tourController.js
+++ 计算机/OpenCourse/Nodejs_express_mongodb_bootcamp/Code/8/controllers/tourController.js
@@ -30,7 +30,7 @@ exports.createTours = async (req, res) => {
   } catch (error) {
     res.status(400).json({
       status: 'fail',
-      message: 'Invalid data sent',
+      message: error,
     });
   }
 };
