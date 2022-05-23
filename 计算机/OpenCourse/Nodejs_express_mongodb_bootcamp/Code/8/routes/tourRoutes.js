5898fd7ce4906e6413ef646ddadea82663790e11
andyfree96 <anthonyfree96@gmail.com>
2022-05-23 21:30:41 +0800

    Making the API Better - Aliasing

--- 计算机/OpenCourse/Nodejs_express_mongodb_bootcamp/Code/8/routes/tourRoutes.js
+++ 计算机/OpenCourse/Nodejs_express_mongodb_bootcamp/Code/8/routes/tourRoutes.js
@@ -4,6 +4,10 @@ const tourController = require('../controllers/tourController');
 const tourRouter = express.Router();
 
 tourRouter
+  .route('/top-5-cheap')
+  .get(tourController.aliasTopTours, tourController.getTours);
+
+tourRouter
   .route('/')
   .get(tourController.getTours)
   .post(tourController.createTours);
