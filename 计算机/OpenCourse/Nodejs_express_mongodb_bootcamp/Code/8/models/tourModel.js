8688b97a7fa1d8fed37116677df9115486f61eef
andyfree96 <anthonyfree96@gmail.com>
2022-05-27 14:56:08 +0800

    Aggregation Middleware

--- 计算机/OpenCourse/Nodejs_express_mongodb_bootcamp/Code/8/models/tourModel.js
+++ 计算机/OpenCourse/Nodejs_express_mongodb_bootcamp/Code/8/models/tourModel.js
@@ -92,5 +92,12 @@ tourSchema.post(/^find/, function (docs, next) {
   next();
 });
 
+// Aggregate middleware
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+  console.log(this.pipeline());
+  next();
+});
+
 const Tour = mongoose.model('Tour', tourSchema);
 module.exports = Tour;
